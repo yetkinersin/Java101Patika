af77e651a24e5416c5b845b195576c81a6df984f
Yetkin Ersin <134930002+yetkinersin@users.noreply.github.com>
2023-05-29 14:48:10 +0300

    Add files via upload

---
+++ Taksimetre.java
@@ -0,0 +1,19 @@
+package Patika3;
+
+import java.util.Scanner;
+public class Taksimetre{
+    public static void main(String[] args) {
+        int km;
+        double perKm = 2.20, total, startPrice = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Mesafeyi KM cinsinden giriniz : ");
+        km = input.nextInt();
+
+        total = (km * perKm);
+        total += startPrice;
+
+        total = (total < 20) ? 20 : total;
+        System.out.println("Toplam Tutar : " + total);
+    }
+}
